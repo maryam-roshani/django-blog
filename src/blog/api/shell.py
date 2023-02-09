4733a9fb6c3b8402312611a0586bc3eeabe8176b
Maryam Roshani <maryamroshani777@gmail.com>
2023-02-09 16:23:16 +0330

    python shell

--- src/blog/api/shell.py
+++ src/blog/api/shell.py
@@ -11,3 +11,16 @@ obj.title
 obj_data = ModelSerializer(obj)
 obj_data.data 
 obj_data.data['one of the fields in ModelSerializer']
+
+data = {
+		"topic" : "history" ,
+		"title" : "world war" ,
+		"text" : "today is the day." ,
+}
+
+new_item = ModelSerializer(data=data)
+new_item.initial_data
+if new_item.is_valid():
+	new_item.save()
+else:
+	print(new_item.errors)
