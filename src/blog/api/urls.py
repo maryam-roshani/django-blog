3f899ec9c202e5ccea6c73b06122bd1d653dcfdd
Maryam Roshani <maryamroshani777@gmail.com>
2023-02-09 12:51:26 +0330

    serializer

--- src/blog/api/urls.py
+++ src/blog/api/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:page>', views.PostListAPIView.as_view(), name='home'),
-    path('create', views.post_create_view, name='create'),
-    path('<str:slug>', views.post_detail, name='detail'),
-    path('show-reply/<str:pk>', views.show_reply_view, name='show-reply'),
-    path('comment-create/<str:pk>', views.comment_create_view, name='create-comment'), 
+    path('', views.PostListAPIView.as_view(), name='home'),
+    # path('create', views.post_create_view, name='create'),
+    # path('<str:slug>', views.post_detail, name='detail'),
+    # path('show-reply/<str:pk>', views.show_reply_view, name='show-reply'),
+    # path('comment-create/<str:pk>', views.comment_create_view, name='create-comment'), 
 ]
