bc191b0f7d57f5f46ba241f7bdda2ff09b02aef0
Maryam Roshani <maryamroshani777@gmail.com>
2023-02-09 16:42:10 +0330

    python shell

--- src/blog/api/serializers.py
+++ src/blog/api/serializers.py
@@ -2,13 +2,23 @@ from rest_framework.serializers import ModelSerializer
 from blog.models import Post
 
 
-class PostSerializer(ModelSerializer):
+class PostListSerializer(ModelSerializer):
 	class Meta:
 		model = Post
 		fields = [
 			'topic',
 			'title',
 			'text',
+		]
+
+
+class PostDetailSerializer(ModelSerializer):
+	class Meta:
+		model = Post
+		fields = [
+			'id',
+			'topic',
+			'title',
+			'text',
 			'slug',
 		]
-		
--- src/blog/api/urls.py
+++ src/blog/api/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.PostListAPIView.as_view(), name='home'),
     # path('create', views.post_create_view, name='create'),
-    # path('<str:slug>', views.post_detail, name='detail'),
+    path('/<str:abc>/', views.PostDetailAPIView.as_view(), name='detail'),
     # path('show-reply/<str:pk>', views.show_reply_view, name='show-reply'),
     # path('comment-create/<str:pk>', views.comment_create_view, name='create-comment'), 
 ]
