fda47ee8415922d79708884dc8aa22eebb536013
Maryam Roshani <maryamroshani777@gmail.com>
2023-02-09 12:35:05 +0330

    list api view

---
+++ src/blog/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('<int:page>', views.PostListAPIView.as_view(), name='home'),
+    path('create', views.post_create_view, name='create'),
+    path('<str:slug>', views.post_detail, name='detail'),
+    path('show-reply/<str:pk>', views.show_reply_view, name='show-reply'),
+    path('comment-create/<str:pk>', views.comment_create_view, name='create-comment'), 
+]
--- src/dblog/urls.py
+++ src/dblog/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', include('blog.urls')),
+    path ('api/blog', include('blog.api.urls'), name='blog-api'),
     path('accounts/', include('allauth.urls')),
 ]
 
