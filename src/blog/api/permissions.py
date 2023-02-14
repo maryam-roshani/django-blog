433cd852a86d0b5bcc648b599963172c18880290
Maryam Roshani <maryamroshani777@gmail.com>
2023-02-14 14:51:36 +0330

    safe method permission

--- src/blog/api/permissions.py
+++ src/blog/api/permissions.py
@@ -6,6 +6,13 @@ ERROR_MESSAGE = 'You must be the owner of this object.'
 
 class IsOwnerOrReadOnly(BasePermission):
 	message = ERROR_MESSAGE
+	my_safe_method = ['GET']
+
+	def has_permission(self, request, view):
+		if request.method in self.my_safe_method:
+			return True
+		return False
+
 	def has_object_permission(self, request, view, obj):
 		if obj.writer != request.user:
 			raise exceptions.PermissionDenied(detail=ERROR_MESSAGE)
