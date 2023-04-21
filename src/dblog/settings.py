40bdd91b597aa005a998783441c3a598270e403e
Maryam Roshani <maryamroshani777@gmail.com>
2023-04-21 05:01:43 +0430

    changing the database

--- src/dblog/settings.py
+++ src/dblog/settings.py
@@ -89,9 +89,18 @@ WSGI_APPLICATION = 'dblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres',
+        'PASSWORD': 'maryam',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
